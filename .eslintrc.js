1526f44bab8eeaebf236f96786267423bf5d5882
BangHyeonJun <davidbang@cj.net>
2024-02-02 13:05:34 +0900

    @repo를 @pfe로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 // 이 구성은 패키지 관리자 루트에만 적용됩니다
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
+	ignorePatterns: ["apps/**", "packages/**"],
+	extends: ["@pfe/eslint-config/library.js"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+	},
 };
