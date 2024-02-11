ffc73cc39583c3a2a1ee18bff0e33f2c2ef732ba
BangHyeonJun <davidbang@cj.net>
2024-02-11 14:49:32 +0900

    shadcn 적용

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@pfe/ui/components/ui/button";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@pfe/ui/components/ui/avatar";
 
 export default function Page() {
-	return (
-		<main>
-			<Button>Click me</Button>
-		</main>
-	);
+  return (
+    <main>
+      <Button>Click me</Button>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </main>
+  );
 }
